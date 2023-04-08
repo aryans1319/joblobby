6343e4fccee1b12580fd5424263570204c6f9f4d
Aryan <aryan4p130202@gmail.com>
2023-04-08 19:29:37 +0530

    [feature]: layout loading only when fonts are loaded first

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
-}
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+  /**
+   *    Layout is loaded only when fonts are loaded
+   *    This is done to prevent the app from crashing 
+   */
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) return null;
+  return <Stack />;
+};
 
 export default Layout;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
 import styles from './welcome.style'
+import { TextInput } from 'react-native-gesture-handler'
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Aryan</Text>
+        <Text style={styles.welcomeMessage}>Find your Perfect Job</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            />
+        </View>
+      </View>
     </View>
   )
 }
